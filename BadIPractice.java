305e20df4016bb757c56898af32ff250373a162c
Nirmalie Perera <nirmalieshehani27@gmail.com>
2026-02-08 11:47:48 +0530

    Interface Segregation Principle

--- BadIPractice.java
+++ BadIPractice.java
@@ -1,37 +1,44 @@
 public class BadIPractice {
-    //interface Document {
-void print();
-void scan();
-void fax();
+    // interface Document {
+    void print();
+
+    void scan();
+
+    void fax();
 }
+
 class SimplePrinter implements Document {
-@Override
-public void print() {
-// Print the document
-}
+    @Override
+    public void print() {
+        // Print the document
+    }
+
 @Override
 public void scan() {
 // Not applicable for
 SimplePrinter
 }
+
 @Override
 public void fax() {
 // Not applicable for
 SimplePrinter
-}
-}
-class Photocopier implements Document {
-@Override
-public void print() {
-// Print the document
-}
-@Override
-public void scan() {
-// Scan the document
-}
-@Override
-public void fax() {
-// Fax the document
 }
 }
 
+class Photocopier implements Document {
+    @Override
+    public void print() {
+        // Print the document
+    }
+
+    @Override
+    public void scan() {
+        // Scan the document
+    }
+
+    @Override
+    public void fax() {
+        // Fax the document
+    }
+}
